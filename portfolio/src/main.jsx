0b52505def8fd8a1787a93e52c6ef67b3b75cb92
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-05 19:55:55 +0100

    Added small amount of content to each page and aded index.html file back

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,25 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/about" index element={<About />} />
-        <Route path="/projects" index element={<Projects />} />
-        <Route path="/contact" index element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
-
-reportWebVitals();
