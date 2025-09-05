0668f0b38e444fb83a6336347ea64df99a1b0cc3
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-05 21:40:45 +0100

    Added folder and files for profile component for the home page

---
+++ portfolio/src/components/profile/Profile.jsx
@@ -0,0 +1,10 @@
+
+function Profile () {
+    return (
+        <section>
+            <h1>Hi there, I'm Abbie</h1>
+        </section>
+    )
+}
+
+export default Profile;
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/navbar/Navbar.jsx";
+import Profile from "../components/profile/Profile.jsx"
 
 function Home() {
   return (
     <>
       <Navbar />
+      <Profile/>
     </>
   );
 }
