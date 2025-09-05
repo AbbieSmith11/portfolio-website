686ddb0a405181a934b6d0df9f5bbb4d18172a4c
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-05 19:15:00 +0100

    Initial set up of routing

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" index element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+reportWebVitals();
