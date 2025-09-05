a831f1d33be00ef8f4d5fc2014e78f8ae56b7150
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-05 20:27:59 +0100

    updated routing and styling for navbar

--- portfolio/src/components/navbar/nav-items/NavItem.jsx
+++ portfolio/src/components/navbar/nav-items/NavItem.jsx
@@ -1,10 +1,9 @@
 import '../Navbar.jsx'
-import { Link } from 'react-router-dom'
 
 function NavItem (props) {
     return (
         <li>
-            <Link to={props.route}>{props.item}</Link>
+            <a href={props.route}>{props.item}</a>
         </li>
     )
 }
