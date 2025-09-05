a5149316fae906e66ac114fbd11761614171bb26
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-05 19:29:53 +0100

    created a pages folder and deleted unnecesary files. Also updated routing for new pages

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,16 +1,22 @@
-import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
 
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" index element={<App />} />
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" index element={<About />} />
+        <Route path="/projects" index element={<Projects />} />
+        <Route path="/contact" index element={<Contact />} />
       </Routes>
     </Router>
   </React.StrictMode>
