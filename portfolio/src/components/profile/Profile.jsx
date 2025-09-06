9f020b0b1aad85019581e72e51b66fe7a3079ae1
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-06 17:33:12 +0100

    Added content and styling for home page

--- portfolio/src/components/profile/Profile.jsx
+++ portfolio/src/components/profile/Profile.jsx
@@ -2,7 +2,7 @@ import './Profile.css'
 
 function Profile () {
     return (
-        <section>
+        <section id='profile'>
             <h1>Hello, I'm Abbie</h1>
             <p> I am a Software Engineer thats passionate about solving complex problems and building purposeful, impactful applications.</p>
         </section>
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar/Navbar.jsx";
 import Profile from "../components/profile/Profile.jsx";
+import AboutMe from "../components/aboutMe/AboutMe.jsx";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Profile />
+      <AboutMe />
     </>
   );
 }
