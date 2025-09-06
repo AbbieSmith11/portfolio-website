47a91b9497ea1f9446a5684ef6bc462850a86099
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-06 15:13:56 +0100

    Decided to place about page on the home page. Deleted the about page

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,22 +1,16 @@
-import './Navbar.css';
-import NavItem from './nav-items/NavItem';
+import "./Navbar.css";
+import NavItem from "./nav-items/NavItem";
 
-function Navbar (){
-
- return (
-
-<nav>
-<ul>
-    <NavItem label='Home' to='/'/>
-    <NavItem label='About' to='/about' />
-    <NavItem label='Projects' to='/projects' />
-    <NavItem label='Contact' to='/contact'/>
-</ul>
-</nav>
-
-)   
-
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <NavItem label="Home" to="/" />
+        <NavItem label="Projects" to="/projects" />
+        <NavItem label="Contact" to="/contact" />
+      </ul>
+    </nav>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
-import About from "./pages/about/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 
@@ -14,7 +13,6 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
