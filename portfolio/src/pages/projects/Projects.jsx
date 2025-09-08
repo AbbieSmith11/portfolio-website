05bafed1eedf8d9d6f747f828a951b263e1eb062
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-08 15:40:50 +0100

    Added animations to project pages

--- portfolio/src/pages/projects/Projects.jsx
+++ portfolio/src/pages/projects/Projects.jsx
@@ -13,6 +13,8 @@ function Projects() {
 
 <div id="fade">
   <h1>My Projects</h1>
+
+  <div id='full'>
   <Project
     name="Earth Angel Club"
     description="EarthAngel.Club is an website designed to support women on their journeys of self-love, healing, and personal growth. The website offers free resources and inspiration to help users reconnect with their inner strength and confidence."
@@ -22,6 +24,7 @@ function Projects() {
     <Tool name="HTML" colour="#FF5A5F" />
     <Tool name="CSS" colour="#B8B4D9" />
     <Tool name="Javascript" colour="#F7C6D9" />
+  </div>
   </div>
   
 </div>
