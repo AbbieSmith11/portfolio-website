5c1967927afc9a3eb044cb7b769f9c1beaa87d90
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-14 19:48:08 +0100

    Changed path

--- portfolio/src/pages/home/Home.jsx
+++ portfolio/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/navbar/Navbar.jsx";
 import Profile from "../../components/profile/Profile.jsx";
 import AboutMe from "../../components/aboutMe/AboutMe.jsx";
 import Hobbies from "../../components/hobbies/Hobbies.jsx";
-import Skills from "../../components/skills/Skills.jsx";
+import Skills from "../../components/skills/skills.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import "./Home.css";
 
