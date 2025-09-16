bb376101ee1bafb94c56ed1e1a764dc36108798e
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-16 16:01:18 +0100

    Fixed animation issue

--- portfolio/src/components/profile/Profile.jsx
+++ portfolio/src/components/profile/Profile.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import './Profile.css'
 
 function Profile () {
+    const [write, setWrite] = useState(false);
+
+    useEffect(()=>{
+        document.fonts.ready.then(()=>{
+            setWrite(true)
+        });
+    }, []);
+
     return (
         <section id='profile'>
-            <h1 id='intro'>Hello, I'm Abbie</h1>
+            <h1 id='intro' className={write ? "write" : ""}>Hello, I'm Abbie</h1>
             <p> I am a Software Engineer that's passionate about solving complex problems and building purposeful, impactful applications.</p>
         </section>
 
